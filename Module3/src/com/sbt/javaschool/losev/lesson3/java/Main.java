3f3debb1fb4d889a527cc17792eb361030a73a54
= <=>
2019-11-11 06:36:46 +0300

    create module

---
+++ Module3/src/com/sbt/javaschool/losev/lesson3/java/Main.java
@@ -0,0 +1,15 @@
+package com.sbt.javaschool.losev.lesson3.java;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.FileSystemNotFoundException;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
