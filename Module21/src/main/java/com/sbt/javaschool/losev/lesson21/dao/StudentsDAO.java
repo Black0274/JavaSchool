498753d8faa13c079e18a33da469077a2174424f
= <=>
2020-02-04 19:42:17 +0300

    StudentsDAO completed

--- Module21/src/main/java/com/sbt/javaschool/losev/lesson21/dao/StudentsDAO.java
+++ Module21/src/main/java/com/sbt/javaschool/losev/lesson21/dao/StudentsDAO.java
@@ -52,16 +52,50 @@ public class StudentsDAO implements DAO<Students> {
 
     @Override
     public Students getById(int id) {
-        return null;
+        String query = "SELECT * FROM STUDENTS WHERE ID=?";
+        Students students = new Students();
+        try (Connection connection = Util.getConnection()){
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            students.setId(resultSet.getInt(1));
+            students.setFirst_name(resultSet.getString(2));
+            students.setSurname(resultSet.getString(3));
+            students.setYear(resultSet.getInt(4));
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return students;
     }
 
     @Override
     public void update(Students students) {
+        String query = "UPDATE STUDENTS SET ID=?, FIRST_NAME=?, SURNAME=?, YEAR=?";
+        try (Connection connection = Util.getConnection()){
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
 
+            preparedStatement.setInt(1, students.getId());
+            preparedStatement.setString(2, students.getFirst_name());
+            preparedStatement.setString(3, students.getSurname());
+            preparedStatement.setInt(4, students.getYear());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void remove(Students students) {
-
+        String query = "DELETE FROM STUDENTS WHERE ID=?";
+        try (Connection connection = Util.getConnection()){
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, students.getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
